1d1dacb192f14ffba09b255082c83a22fe671f4a
zahariaalexandra <62277649+zahariaalexandra@users.noreply.github.com>
2020-04-12 19:17:27 +0300

    .

--- CNA-tema1-Client/Program.cs
+++ CNA-tema1-Client/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using CNA_tema1_Server;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
 
 namespace CNA_tema1_Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = new HelloRequest { Name = "Tim" };
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
         }
     }
 }
