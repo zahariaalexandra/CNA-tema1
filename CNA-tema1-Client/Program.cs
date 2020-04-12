4a8e1f52cbd65a3a6e81e759390f1361e69e3d81
zahariaalexandra <62277649+zahariaalexandra@users.noreply.github.com>
2020-04-12 18:32:15 +0300

    Add server & client

---
+++ CNA-tema1-Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CNA_tema1_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
